a35c3bcd0f84c7c0cf168c1a8fa915a411aa782d
Abygail27 <abygaailrrocha@gmail.com>
2019-06-25 12:39:47 -0500

    Vunue NFO component zoom transition on 500

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header'; 
 import Featured from './components/featured';
-
+import VunueNfo from './components/vunueNfo';
 
 function App() {
   return (
     <div className="App" style = {{ height: "1500px", background: 'cornflowerblue'}}>
   <Header/>
   <Featured/>
+  <VunueNfo/>
     </div>
   );
 }
