cbfa5f51e7f3541e3580e425659ba0e791d0c1b5
Abygail27 <abygaailrrocha@gmail.com>
2019-06-25 12:49:45 -0500

    Highlight component with text fades when pages reloads

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header'; 
 import Featured from './components/featured';
 import VunueNfo from './components/vunueNfo';
+import Hightlight from './components/Highlights'
+class App extends Component {
+  render () {
 
-function App() {
-  return (
-    <div className="App" style = {{ height: "1500px", background: 'cornflowerblue'}}>
-  <Header/>
-  <Featured/>
-  <VunueNfo/>
-    </div>
-  );
-}
+    return (
+      <div className="App" style = {{ height: "1500px", background: 'cornflowerblue'}}>
+    <Header/>
+    <Featured/>
+    <VunueNfo/>
+    <Hightlight/>
+      </div>
+    );
+  }
+  }
 
 export default App;
---
+++ src/components/Highlights/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Description from './Description';
+
+const Hightlights = () => {
+    return (
+        <div className = "highlight_wrapper">
+             <Description/>
+            
+        </div>
+    );
+};
+
+export default Hightlights;
